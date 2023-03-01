f4f54ea4fa31e4464c7832608cc9ab3426bdd430
Julian Gruber <julian@juliangruber.com>
2023-03-01 13:09:47 +0100

    refactor

--- bin/station.js
+++ bin/station.js
@@ -4,6 +4,7 @@ import { join } from 'node:path'
 import { homedir } from 'node:os'
 import { mkdir } from 'node:fs/promises'
 import * as saturnNode from '../lib/saturn-node.js'
+import { createWriteStream } from 'node:fs'
 
 const {
   FIL_WALLET_ADDRESS,
@@ -27,6 +28,9 @@ await mkdir(paths.moduleLogs, { recursive: true })
 await saturnNode.start({
   FIL_WALLET_ADDRESS,
   storagePath: join(paths.moduleStorage, 'saturn-L2-node'),
-  logPath: join(paths.moduleLogs, 'saturn-L2-node.log'),
-  metricsPath: paths.metrics
+  metricsPath: paths.metrics,
+  logStream: createWriteStream(
+    join(paths.moduleLogs, 'saturn-L2-node.log'),
+    { flags: 'a' }
+  )
 })
