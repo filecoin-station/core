e3bd132fbf05b44aba816fd58abb6b6c2c3a7efd
Julian Gruber <julian@juliangruber.com>
2023-03-06 13:00:36 +0100

    update

--- commands/metrics.js
+++ commands/metrics.js
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises'
 import { Tail } from 'tail'
 import { paths } from '../lib/paths.js'
 
-const formatMetrics = (metrics) =>
+const formatMetricsLogLine = (metrics) =>
   JSON.stringify(
     JSON.parse(metrics.split(/\[([^\]]+)\] /).pop()),
     0,
@@ -14,9 +14,9 @@ export const metrics = async () => {
     process.argv.includes('--follow')
   if (isFollow) {
     const tail = new Tail(paths.metrics, { nLines: 1 })
-    tail.on('line', line => console.log(formatMetrics(line)))
+    tail.on('line', line => console.log(formatMetricsLogLine(line)))
   } else {
     const metrics = await fs.readFile(paths.metrics, 'utf-8')
-    console.log(formatMetrics(metrics.trim().split('\n').pop()))
+    console.log(formatMetricsLogLine(metrics.trim().split('\n').pop()))
   }
 }
