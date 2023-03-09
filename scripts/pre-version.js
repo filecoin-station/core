bf823de46faac240145012341e96e5cfb125b74f
Julian Gruber <julian@juliangruber.com>
2023-03-09 11:12:48 +0100

    pkg: fix pre-version script

--- scripts/pre-version.js
+++ scripts/pre-version.js
@@ -3,8 +3,10 @@
 import { join, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import fs from 'node:fs/promises'
+import { execa } from 'execa'
 
 const repoRoot = join(dirname(fileURLToPath(import.meta.url)), '..')
 const pkg = JSON.parse(await fs.readFile(join(repoRoot, 'package.json')))
 pkg.sentryEnvironment = 'production'
 await fs.writeFile(join(repoRoot, 'package.json'), JSON.stringify(pkg, 0, 2))
+await execa('git', ['add', 'package.json'])
