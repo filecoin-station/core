316c80b14caeb0a78105a913ff1403e5d33647a3
Julian Gruber <julian@juliangruber.com>
2023-03-14 10:03:54 +0100

    add write to activity log

--- commands/station.js
+++ commands/station.js
@@ -3,6 +3,7 @@ import { paths } from '../lib/paths.js'
 import * as saturnNode from '../lib/saturn-node.js'
 import { createLogStream } from '../lib/log.js'
 import { createMetricsStream } from '../lib/metrics.js'
+import { createActivityStream } from '../lib/activity.js'
 
 const { FIL_WALLET_ADDRESS } = process.env
 
@@ -17,6 +18,7 @@ export const station = async () => {
     storagePath: join(paths.moduleStorage, 'saturn-L2-node'),
     binariesPath: paths.moduleBinaries,
     metricsStream: createMetricsStream(paths.metrics),
+    activityStream: createActivityStream(),
     logStream: createLogStream(join(paths.moduleLogs, 'saturn-L2-node.log'))
   })
 }
