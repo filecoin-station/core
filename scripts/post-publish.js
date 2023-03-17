c695db2e8dde1a231eed569a24aecfb4e26961e8
Julian Gruber <julian@juliangruber.com>
2023-03-17 11:36:39 +0100

    pkg: fix missing newline at EOF

--- scripts/post-publish.js
+++ scripts/post-publish.js
@@ -8,7 +8,10 @@ import fs from 'node:fs/promises'
 const repoRoot = join(dirname(fileURLToPath(import.meta.url)), '..')
 const pkg = JSON.parse(await fs.readFile(join(repoRoot, 'package.json')))
 pkg.sentryEnvironment = 'development'
-await fs.writeFile(join(repoRoot, 'package.json'), JSON.stringify(pkg, 0, 2))
+await fs.writeFile(
+  join(repoRoot, 'package.json'),
+  JSON.stringify(pkg, 0, 2) + '\n'
+)
 await execa('git', ['add', 'package.json'])
 await execa('git', ['commit', '-m', 'chore: set sentry environment to development'])
 await execa('git', ['push'])
