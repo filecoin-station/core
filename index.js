11f6eb4cd33b6dc031d6c6716a3f68d4632eed6e
Julian Gruber <julian@juliangruber.com>
2023-04-20 11:54:23 +0200

    add api (wip)

---
+++ index.js
@@ -0,0 +1,18 @@
+import { getActivity, followActivity } from './lib/activity.js'
+import { getLatestLogs, followLogs } from './lib/log.js'
+import { getLatestMetrics, followMetrics } from './lib/metrics'
+
+export const core = {
+  activity: {
+    get: getActivity,
+    follow: followActivity
+  },
+  logs: {
+    get: getLatestLogs,
+    follow: followLogs
+  },
+  metrics: {
+    getLatest: getLatestMetrics,
+    follow: followMetrics
+  }
+}
