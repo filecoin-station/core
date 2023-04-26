e31bb4dc47debd4de09af75ec52e25f6a467e0b9
Miroslav Bajtoš <oss@bajtos.net>
2023-04-26 11:17:07 +0200

    update tests
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- test/station.js
+++ test/station.js
@@ -7,20 +7,30 @@ import { randomUUID } from 'node:crypto'
 import { join } from 'node:path'
 
 describe('Station', () => {
-  it('runs Saturn', async () => {
+  it('runs Saturn and Zinnia', async () => {
     const CACHE_ROOT = join(tmpdir(), randomUUID())
     const STATE_ROOT = join(tmpdir(), randomUUID())
     const ps = execa(
       station,
       { env: { CACHE_ROOT, STATE_ROOT, FIL_WALLET_ADDRESS } }
     )
+    ps.stderr.pipe(process.stderr)
     assert.strictEqual(
       (await once(ps.stdout, 'data'))[0].toString().trim(),
       JSON.stringify({ totalJobsCompleted: 0, totalEarnings: '0' }, 0, 2)
     )
-    ps.stderr.pipe(process.stderr)
+    const messages = []
+    for await (const line of ps.stdout) {
+      messages.push(line.toString().trim())
+      if (messages.length === 2) break
+    }
+
     assert.match(
-      (await once(ps.stdout, 'data'))[0].toString().trim(),
+      messages[0],
+      /^\[.+\] INFO {2}Module Runtime started.$/
+    )
+    assert.match(
+      messages[1],
       /^\[.+\] INFO {2}Saturn Node will try to connect to the Saturn Orchestrator\.\.\.$/
     )
     ps.kill()
@@ -36,11 +46,15 @@ describe('Station', () => {
     const events = []
     for await (const line of ps.stdout) {
       events.push(line.toString().trim())
-      if (events.length === 2) break
+      if (events.length === 3) break
     }
     ps.kill()
     assert.strictEqual(events[0], '{\n  "totalJobsCompleted": 0,\n  "totalEarnings": "0"\n}')
-    assert.match(events[1], /^\[.+\] INFO {2}Saturn Node will try to connect to the Saturn Orchestrator\.\.\.$/)
+    const logs = events.slice(1)
+    logs.sort()
+
+    assert.match(logs[0], /^\[.+\] INFO {2}Module Runtime started.$/)
+    assert.match(logs[1], /^\[.+\] INFO {2}Saturn Node will try to connect to the Saturn Orchestrator\.\.\.$/)
   })
   it('outputs events json', async () => {
     const CACHE_ROOT = join(tmpdir(), randomUUID())
@@ -53,15 +67,29 @@ describe('Station', () => {
     const events = []
     for await (const line of ps.stdout) {
       events.push(JSON.parse(line.toString()))
-      if (events.length === 2) break
+      if (events.length === 3) break
     }
     ps.kill()
     assert(events[1].timestamp)
     delete events[1].timestamp
     assert(events[1].id)
     delete events[1].id
+    assert(events[2].timestamp)
+    delete events[2].timestamp
+    assert(events[2].id)
+    delete events[2].id
+    events.sort((a, b) => {
+      const left = `${a.type}:${a.module ?? ''}:${a.message}`
+      const right = `${a.type}:${a.module ?? ''}:${a.message}`
+      return left > right ? 1 : left < right ? -1 : 0
+    })
     assert.deepStrictEqual(events, [
       { type: 'jobs-completed', total: 0 },
+      {
+        type: 'activity:info',
+        module: 'Runtime',
+        message: 'Module Runtime started.'
+      },
       {
         type: 'activity:info',
         module: 'Saturn',
