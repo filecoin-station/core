f90f67de47031282fc59bf28a6d94b6e0b15560b
Julian Gruber <julian@juliangruber.com>
2023-04-27 12:14:02 +0200

    default contructor args

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { Activity } from './lib/activity.js'
 import { Logs } from './lib/log.js'
 import { Metrics } from './lib/metrics.js'
-import { getPaths } from './lib/paths.js'
+import { getPaths, getDefaultRootDirs } from './lib/paths.js'
 
 export class Core {
-  constructor ({ cacheRoot, stateRoot }) {
+  constructor ({ cacheRoot, stateRoot } = getDefaultRootDirs()) {
     const paths = this.paths = getPaths({ cacheRoot, stateRoot })
     const logs = this.logs = new Logs(paths)
     this.activity = new Activity({ paths, logs })
