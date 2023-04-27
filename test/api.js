4759753fc15abe239172bc3e77098899401d2b87
Julian Gruber <julian@juliangruber.com>
2023-04-27 12:14:07 +0200

    add smoke tests

--- test/api.js
+++ test/api.js
@@ -3,10 +3,36 @@ import assert from 'node:assert'
 import { getDefaultRootDirs } from '../lib/paths.js'
 
 describe('API', () => {
-  it('can be imported', () => {
-    assert(Core)
+  describe('Core()', () => {
+    it('can be imported', () => {
+      assert(Core)
+    })
+    it('can be constructed', () => {
+      assert(new Core())
+    })
+    it('can be constructed with custom paths', () => {
+      assert(new Core(getDefaultRootDirs()))
+    })
   })
-  it('can be constructed', () => {
-    assert(new Core(getDefaultRootDirs()))
+  describe('core.logs', () => {
+    it('exports methods', () => {
+      const core = new Core()
+      assert(core.logs.get)
+      assert(core.logs.follow)
+    })
+  })
+  describe('core.activity', () => {
+    it('exports methods', () => {
+      const core = new Core()
+      assert(core.activity.get)
+      assert(core.activity.follow)
+    })
+  })
+  describe('core.metrics', () => {
+    it('exports methods', () => {
+      const core = new Core()
+      assert(core.metrics.getLatest)
+      assert(core.metrics.follow)
+    })
   })
 })
