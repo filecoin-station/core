f901e4c33049e1a378eb70b7bc85838dbd2a3efa
Julian Gruber <julian@juliangruber.com>
2023-05-25 13:02:16 +0200

    add passing test for metrics deduplication

--- test/metrics.js
+++ test/metrics.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const assert = require('node:assert')
-const { metrics } = require('../lib/metrics')
+const { Metrics } = require('../lib/metrics')
 
 describe('Metrics', () => {
   describe('submit', () => {
     it('should merge metrics', () => {
+      const metrics = new Metrics()
       metrics.submit('module1', {
         totalJobsCompleted: 1,
         totalEarnings: '0'
@@ -16,6 +17,38 @@ describe('Metrics', () => {
       })
       assert.deepStrictEqual(metrics.mergedMetrics, {
         totalJobsCompleted: 3,
+        totalEarnings: '0'
+      })
+    })
+    it('should filter duplicate entries', () => {
+      const metrics = new Metrics()
+      let i = 0
+      metrics.onUpdate(metrics => {
+        if (i === 0) {
+          assert.deepStrictEqual(metrics, {
+            totalJobsCompleted: 1,
+            totalEarnings: '0'
+          })
+        } else if (i === 1) {
+          assert.deepStrictEqual(metrics, {
+            totalJobsCompleted: 2,
+            totalEarnings: '0'
+          })
+        } else {
+          throw new Error('should not be called')
+        }
+        i++
+      })
+      metrics.submit('module1', {
+        totalJobsCompleted: 1,
+        totalEarnings: '0'
+      })
+      metrics.submit('module1', {
+        totalJobsCompleted: 1,
+        totalEarnings: '0'
+      })
+      metrics.submit('module2', {
+        totalJobsCompleted: 1,
         totalEarnings: '0'
       })
     })
