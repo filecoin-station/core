5d7ff7b18622b4d7cfaf7f9a724654514f39bdfd
Julian Gruber <julian@juliangruber.com>
2023-05-28 14:37:30 +0200

    `URL.pathname` isn't cross platform

--- test/util.js
+++ test/util.js
@@ -1,4 +1,8 @@
+import { fileURLToPath } from 'node:url'
+
 // From https://lotus.filecoin.io/lotus/manage/manage-fil/
 export const FIL_WALLET_ADDRESS = 'f1abjxfbp274xpdqcpuaykwkfb43omjotacm2p3za'
 
-export const station = new URL('../bin/station.js', import.meta.url).pathname
+export const station = fileURLToPath(
+  new URL('../bin/station.js', import.meta.url)
+)
