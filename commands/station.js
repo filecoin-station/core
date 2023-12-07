f1d549de1d70b6f17b6c9efbe6bb4552d7f5a3a3
patrickwoodhead <patrick.woodhead@protocol.ai>
2023-12-07 10:59:07 +0000

    revert back to parsing ether for the console.log

--- commands/station.js
+++ commands/station.js
@@ -71,7 +71,7 @@ export const station = async ({ json, experimental }) => {
       console.log(JSON.stringify({
         type: 'jobs-completed',
         total: metrics.totalJobsCompleted,
-        rewardsScheduledForAddress: metrics.rewardsScheduledForAddress.toString()
+        rewardsScheduledForAddress: formatEther(metrics.rewardsScheduledForAddress)
       }))
     } else {
       console.log(JSON.stringify({
